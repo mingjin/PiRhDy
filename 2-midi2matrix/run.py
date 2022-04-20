010339b78fa8cf0e5496772cd6e70b87571cad25
Ming Jin <skyairmj@gmail.com>
2022-04-20 11:00:20 +0800

    Ming Jin: bugfix

--- 2-midi2matrix/run.py
+++ 2-midi2matrix/run.py
@@ -1,7 +1,7 @@
 from functions import midi_to_matrix, write_filenames
 dirs=['0']#,'1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 file_path = '../dataset/lakh_normalized/'  # lmd dataset path
-name_path = '../filenames/lakh_dataset/'
+name_path = '../filenames/lakh_normalized/'
 write_filenames(file_path, dirs, name_path, True)
 for name in dirs:
     midi_to_matrix(name)
