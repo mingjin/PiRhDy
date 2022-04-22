549e54a4e669476c9a30a460f744d8cd44ab76f5
Ming Jin <skyairmj@gmail.com>
2022-04-22 17:05:29 +0800

    fix dataset path

--- 1-normalization-midi-file/run.py
+++ 1-normalization-midi-file/run.py
@@ -1,6 +1,6 @@
 from functions import normalization_midi_file, write_filenames
 dirs=['0']#,'1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-file_path = '../../lmd_full/'
+file_path = '../dataset/lakh_dataset/'
 name_path = '../filenames/lakh_dataset/'
 write_filenames(file_path, dirs, name_path, True)
 normalization_midi_file(dirs)
